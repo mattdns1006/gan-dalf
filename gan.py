de45acbcc687ce8f0a0dd8ec6e3d4fc46f155519
matt <mattdns100689@gmail.com>
2018-08-05 20:45:43 +0100

    some updates

--- gan.py
+++ gan.py
@@ -12,6 +12,8 @@ import matplotlib
 matplotlib.use('Agg')
 import sys, pdb, cv2, os
 from loader import Loader
+import seaborn as sns
+sns.set()
 
 os.environ["CUDA_VISIBLE_DEVICES"]="1" 
 
@@ -27,7 +29,7 @@ class GAN():
         self.h = self.img_shape[0]
         self.w = self.img_shape[1]
         self.channels = self.img_shape[2]
-        self.latent_dim = 100
+        self.latent_dim = 40
 
         optimizer = Adam(0.0002, 0.5)
 
@@ -53,10 +55,10 @@ class GAN():
 
         ConvT = Conv2DTranspose
         model = Sequential()
-        model.add(Dense(256*4*4,input_shape=(self.latent_dim,)))
+        model.add(Dense(512*4*4,input_shape=(self.latent_dim,)))
         model.add(LeakyReLU(0.2))
         model.add(BatchNormalization())
-        model.add(Reshape((4,4,256)))
+        model.add(Reshape((4,4,512)))
 
         model.add(ConvT(128,kernel_size=5,strides=2,padding='same'))
         model.add(LeakyReLU(0.2))
@@ -116,8 +118,8 @@ class GAN():
 
         n_batches = 100000
         sample_interval= 1000
-        gen_losses = np.zeros(n_batches) 
-        disc_metrics = np.zeros((n_batches,2))
+        self.gen_losses = np.zeros(n_batches) 
+        self.disc_metrics = np.zeros((n_batches,2))
 
         for i in range(n_batches):
             epoch = self.dgen.epoch
@@ -132,37 +134,31 @@ class GAN():
             # Train the discriminator
             disc_metrics_real = self.discriminator.train_on_batch(imgs, valid)
             disc_metrics_fake = self.discriminator.train_on_batch(gen_imgs, fake)
-            disc_metrics[i] = 0.5 * np.add(disc_metrics_real, disc_metrics_fake)
+            self.disc_metrics[i] = 0.5 * np.add(disc_metrics_real, disc_metrics_fake)
 
             #  Train Generator
             noise = np.random.normal(0, 1, (batch_size, self.latent_dim)) # random noise input
-            gen_losses[i] = self.combined.train_on_batch(noise, valid) # train generator by feeding fake images through discriminator which are likely to produce a valid response
+            self.gen_losses[i] = self.combined.train_on_batch(noise, valid) # train generator by feeding fake images through discriminator which are likely to produce a valid response
 
-            if i > 20:
+            if i > 20 and i % 10 == 0:
                 print ("Epoch %d - batch_no %d [D loss: %f, acc.: %.2f%%] [G loss: %f] (Running means (20obs))" % (epoch,i,
-                    disc_metrics[i-20:i,0].mean(), 100*disc_metrics[i-20:i,1].mean(), gen_losses[i-20:i].mean()))
+                    self.disc_metrics[i-20:i,0].mean(), 100*self.disc_metrics[i-20:i,1].mean(), self.gen_losses[i-20:i].mean()))
 
-            if i % sample_interval == 0:
-                self.save_imgs(imgs,'real')
-                self.save_imgs(gen_imgs,'fake_{0}_{1}_'.format(epoch,i))
+            if i % sample_interval == 0 and i > 0:
                 self.save_imgs(gen_imgs,'fake_latest_'.format(epoch,i))
-            if i % 1000 == 0:
-                print("Saving")
-                self.generator.save('gen.h5')
-                self.combined.save('model.h5')
-                np.save('gen', gen_losses)
-                np.save('disc', disc_metrics)
+                np.save('gen',self.gen_losses)
+                np.save('disc',self.disc_metrics)
+                #self.save_imgs(imgs,'real')
+                #self.generator.save('gen.h5')
+                #self.combined.save('model.h5')
+
 
     def save_imgs(self,arr,ext):
         arr = self.dgen.img_norm(arr,inverse=True).astype(np.uint8)
         for i in range(arr.shape[0]):
             cv2.imwrite("samples/{0}_{1}.jpg".format(ext,i),arr[i])
 
-
-
-
 if __name__ == '__main__':
-    inference = False
     train = True 
     if train == True:
         gan = GAN(load=False)
